47cfa8f6906d1c9b5ae9a5185705c0d55edce148
preetu391 <preetu391391@gmail.com>
2022-11-23 00:25:40 +0530

    company page skeleton added

--- src/Pages/CompanyPage.js
+++ src/Pages/CompanyPage.js
@@ -2,8 +2,19 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from "react-router-dom";
 import { Box } from '@mui/system';
 import axios from 'axios';
-import { Typography } from '@mui/material';
+import { Skeleton, Typography } from '@mui/material';
 import Tooltip from '@mui/material/Tooltip';
+import Grid from '@mui/material/Grid';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+    typography: {
+      fontFamily: [
+        '-apple-system',
+        'BlinkMacSystemFont',
+      ].join(','),
+    },
+  });
 
 const CompanyPage = () => {
 
@@ -29,56 +40,62 @@ const CompanyPage = () => {
         <>
             <Box display="flex" flexDirection="column" sx={{ mt: 5 }}>
                 <Box>
-                    <Box display="flex" flexDirection="row-reverse" alignItems="center">
-                        <Box>
-                            <Box display="flex" flexDirection="column" alignItems="center" justifyContent="flex-end">
-                                <Tooltip arrow title={
-                                    <Box display="flex" flexDirection="column">
-                                        <Box>
-                                            <Typography sx={{ fontWeight: "500" }} variant="body2" >
-                                                Sector: {comp.Sector}
-                                            </Typography>
-                                        </Box>
-                                        <Box>
-                                            <Typography sx={{ fontWeight: "500" }} variant="body2" >
-                                                Industry: {comp.Industry}
-                                            </Typography>
-                                        </Box>
-                                    </Box>
-                                } 
-                                placement="left"
-                                componentsProps={{
-                                    tooltip: {
-                                      sx: {
-                                        bgcolor: '#4d5d53',
-                                        height: 56,
-                                        margin: "auto",
-                                        '& .MuiTooltip-arrow': {
-                                          color: '#4d5d53',
-                                        },
-                                      },
-                                    },
-                                  }}>
+                    <Grid container alignItems="center" justifyContent="center">
+                        <Grid item >
+                            <Tooltip arrow title={
+                                <Box display="flex" flexDirection="column">
                                     <Box>
-                                        <Typography sx={{ ml: 2, mr: 0.5, fontWeight: "600" }} variant="h2" color="black">
-                                            <i>{comp.LongName}</i>
+                                        <Typography sx={{ fontWeight: "500" }} variant="body2" >
+                                            Sector: {comp.Sector}
                                         </Typography>
                                     </Box>
-                                </Tooltip>
-                                <Box>
-                                    <Typography sx={{ ml: 2, mr: 0.5, fontWeight: "500" }} variant="h6" color="black">
-                                        No. of shares: <strong>5</strong>
-                                    </Typography>
+                                    <Box>
+                                        <Typography sx={{ fontWeight: "500" }} variant="body2" >
+                                            Industry: {comp.Industry}
+                                        </Typography>
+                                    </Box>
                                 </Box>
-                            </Box>
-                        </Box>
-                    </Box>
+                            }
+                                placement="right"
+                                componentsProps={{
+                                    tooltip: {
+                                        sx: {
+                                            bgcolor: '#4d5d53',
+                                            height: 56,
+                                            margin: "auto",
+                                            '& .MuiTooltip-arrow': {
+                                                color: '#4d5d53',
+                                            },
+                                        },
+                                    },
+                                }}>
+                                <Box>
+                                    {comp.length === 0 ? <Skeleton variant="rectangular" width={400} height={60} /> : <ThemeProvider theme={theme}><Typography sx={{ ml: {xs: 0, sm: 1}, mr: 0.5, fontWeight: "600", fontSize: "40px" }} variant="h2" color="black">
+                                        {comp.LongName}
+                                    </Typography></ThemeProvider>}
+
+                                </Box>
+                            </Tooltip>
+                        </Grid>
+                        <Grid item xs>
+                            <Grid container direction="row-reverse">
+                                <Grid item>
+                                    <Typography sx={{mr: {xs: 1, sm: 5}, fontWeight: "500" }} variant="body1" color="black">
+                                        Number of shares you own: <strong>5</strong>
+                                    </Typography>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </Grid>
                 </Box>
+            </Box>
+            <Box>
                 company {params.symbol}
             </Box>
-
         </>
     )
 }
 
 export default CompanyPage
+
+
