8d964804b3c763c8e3078faaabeb9428e4b248f5
Aditi <ritikachauhannn123@gmail.com>
2022-11-23 13:18:44 +0530

    adding login signup and addshare components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { Route, Routes, Navigate } from "react-router-dom";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+import AddShare from "./components/AddShare";
 
 function App() {
+  const token = localStorage.getItem("token");
+
   return (
-    <div>
-      Finance Data
-    </div>
+    <Routes>
+      <Route path="/signup" exact element={<Signup />} />
+      <Route path="/login" exact element={<Login />} />
+      <Route path="/addshare" exact element={<AddShare />} />
+    </Routes>
   );
 }
 
